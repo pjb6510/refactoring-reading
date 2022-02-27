1fcbffe8cf56f0460db6a38b5c57ad26d0859329
Jason <jason@protopie.io>
2022-02-27 18:17:28 +0900

    add: htmlStatement in statement3.ts

--- src/chapter1/statement3.ts
+++ src/chapter1/statement3.ts
@@ -3,19 +3,44 @@ import { Plays } from './plays';
 import { StatementData } from './StatementData';
 import { createStatementData } from './createStatementData';
 
-export function statement3(invoice: Invoice, plays: Plays) {
-  function usd(aNumber: number | undefined) {
-    if (aNumber == null) {
-      throw new Error('aNumber is undefined');
-    }
-
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+function usd(aNumber: number | undefined) {
+  if (aNumber == null) {
+    throw new Error('aNumber is undefined');
   }
 
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
+export function htmlStatement(invoice: Invoice, plays: Plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+export function renderHtml(data: StatementData) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>';
+
+  for (let perf of data.performances) {
+    if (perf.play == null) {
+      throw new Error('play is undefined');
+    }
+
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += '</table>\n';
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+  return result;
+}
+
+export function statement3(invoice: Invoice, plays: Plays) {
   function renderPlainText(data: StatementData) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     for (let perf of data.performances) {
